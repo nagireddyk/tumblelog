33ff381b80fc6fddeef7f90461901ee9bc201028
I849548 <shifei.wu@sap.com>
2015-07-27 18:08:42 -0700

    init

---
+++ tumblelog/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import patterns, include, url
+from django.views.generic import ListView, DetailView
+from tumblelog.models import Post
+
+urlpatterns = patterns('',
+    url(r'^$', ListView.as_view(
+        queryset=Post.objects.all(),
+        context_object_name="posts_list"),
+        name="home"
+    ),
+    url(r'^post/(?P<slug>[a-zA-Z0-9-]+)/$', DetailView.as_view(
+        queryset=Post.objects.all(),
+        context_object_name="post"),
+        name="post"
+    ),
+)
